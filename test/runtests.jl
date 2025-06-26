708667a06888b421d76de104eecb532d59dc1d91
emmy <52672675+emmaccode@users.noreply.github.com>
2025-06-26 03:38:08 -0600

    test init

---
+++ test/runtests.jl
@@ -0,0 +1,19 @@
+using ChiDB
+using Test
+@testset "chifi database server" verbose = true begin
+    @testset "internal functions" begin
+
+    end
+    @testset "server start" begin
+
+    end
+    @testset "login" begin
+
+    end
+    @testset "queries" begin
+
+    end
+    @testset "query commands" begin
+
+    end
+end
