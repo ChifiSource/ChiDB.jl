87cf7d3a20cd510b5cf1b8d55dadc2c3a389bde9
emmy <52672675+emmaccode@users.noreply.github.com>
2025-07-08 23:59:41 -0600

    completed `Crypt` addition

--- src/dtypes.jl
+++ src/dtypes.jl
@@ -1,11 +1,19 @@
-struct CryptString
+abstract type AbstractCrypt end
+
+struct CryptString <: AbstractCrypt
     value::String
 end
 
-parse(t::Type{CryptString}, a::AbstractString) = begin
+parse(t::Type{<:AbstractCrypt}, a::AbstractString) = begin
     t(a)
 end
 
 function get_datatype(std::Type{AlgebraStreamFrames.StreamDataType{:Crypt}})
     CryptString
 end
+
+algebra_initializer(this_T::Type{<:AbstractCrypt}) = "null"
+
+string(crypt::AbstractCrypt) = crypt.value::String
+
+abstract type AbstractLong end
